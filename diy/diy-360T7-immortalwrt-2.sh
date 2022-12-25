8fface736ce22a3e6cb4e061584877d605139a3c
wjw9527 <1975499235@qq.com>
2022-12-25 14:45:11 +0000

    Update diy-360T7-immortalwrt-2.sh

--- diy/diy-360T7-immortalwrt-2.sh
+++ diy/diy-360T7-immortalwrt-2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #修改默认IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
 
 # 移除重复软件包
 #rm -rf feeds/luci/themes/luci-theme-argon
@@ -23,7 +23,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-Butterfly/g' feeds/luci/collections/lu
 #git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加额外软件包
 # git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
@@ -34,7 +34,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-Butterfly/g' feeds/luci/collections/lu
 # svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 
 # 科学上网插件依赖
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
@@ -52,12 +52,12 @@ sed -i 's/luci-theme-bootstrap/luci-theme-Butterfly/g' feeds/luci/collections/lu
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
-# svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
-# svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
-# svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
-# svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-# svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-# svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
-# svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
-# svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
-# svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
+svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
+svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
+svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
+svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
+svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
+svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
+svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
+svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
